e8fb17768c48178804dfa92ccb6e4f080ca36a50
YC <yc@ycs-macbook-pro.local>
2016-08-02 15:04:47 -0700

    Initial Commit

---
+++ ParserYalcinCakmakTests/ParserYalcinCakmakTests.swift
@@ -0,0 +1,36 @@
+//
+//  ParserYalcinCakmakTests.swift
+//  ParserYalcinCakmakTests
+//
+//  Created by YC on 8/2/16.
+//  Copyright © 2016 University of California at Santa Cruz. All rights reserved.
+//
+
+import XCTest
+@testable import ParserYalcinCakmak
+
+class ParserYalcinCakmakTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
